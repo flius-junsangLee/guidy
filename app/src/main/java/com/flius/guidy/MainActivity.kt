c41b401e00e443db459e8e0446056e5acd0bca1a
JunsangLee <juoc03@gmail.com>
2024-05-09 05:43:14 +0900

    1

--- app/src/main/java/com/flius/guidy/MainActivity.kt
+++ app/src/main/java/com/flius/guidy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.flius.guidy
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val PostDataList = listOf("대니안", "민재몬", "근육기성", "이준상")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, PostDataList)
+        val listView: ListView = findViewById(R.id.PostListView)
+        listView.adapter = adapter
     }
 }
