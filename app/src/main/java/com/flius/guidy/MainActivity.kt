28db1b4df2807bb371d3735ab395b9e9b69ac134
JunsangLee <juoc03@gmail.com>
2024-05-09 13:31:11 +0900

    1

--- app/src/main/java/com/flius/guidy/MainActivity.kt
+++ app/src/main/java/com/flius/guidy/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.flius.guidy
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var editText: EditText
+    private lateinit var adapter: ArrayAdapter<String>
+    private val listItems = mutableListOf("때니", "빡기성", "민재몬", "이준상")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,9 +27,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val PostDataList = listOf("대니안", "민재몬", "근육기성", "이준상")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, PostDataList)
-        val listView: ListView = findViewById(R.id.PostListView)
+
+        listView = findViewById(R.id.PostListView)
+        editText = findViewById(R.id.tv_addItem)
+        val addButton: Button = findViewById(R.id.bt_profileAdd)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
         listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra("detail", listItems[position])
+            startActivity(intent)
+        }
+        addButton.setOnClickListener {
+            val newItem = editText.text.toString()
+            if (newItem.isNotEmpty()) {
+                listItems.add(newItem)
+                adapter.notifyDataSetChanged()
+                editText.setText("")
+
+
+            }
+        }
+    }
+}
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val textView = findViewById<TextView>(R.id.tv_profile)
+        val detailText = intent.getStringExtra("detail")
+        textView.text = detailText
     }
 }
