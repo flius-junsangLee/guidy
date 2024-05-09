e0cd32adf20f7fcd15ace5807b82c7c50a48b089
JunsangLee <juoc03@gmail.com>
2024-05-09 13:57:18 +0900

    1

--- app/src/main/java/com/flius/guidy/MainActivity.kt
+++ app/src/main/java/com/flius/guidy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.flius.guidy
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ProfileListView: ListView
+    private lateinit var addprofile: EditText
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var addButton: Button
+    private val dataManager = ListDataManager()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,9 +25,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val PostDataList = listOf("대니안", "민재몬", "근육기성", "이준상")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, PostDataList)
-        val listView: ListView = findViewById(R.id.PostListView)
-        listView.adapter = adapter
+
+        ProfileListView = findViewById(R.id.PostListView)
+        addprofile = findViewById(R.id.tv_addItem)
+        addButton = findViewById(R.id.bt_profileAdd)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dataManager.getItems())
+        ProfileListView.adapter = adapter
+
+        setupAddButtonListener()
+
+    }
+    private fun setupAddButtonListener() {
+        addButton.setOnClickListener {
+            val newItem = addprofile.text.toString()
+            if (newItem.isNotEmpty()) {
+                dataManager.addItem(newItem)
+                adapter.notifyDataSetChanged()
+                addprofile.setText("")
+            }
+        }
+    }
+
+}
+class ListDataManager {
+    private val items = mutableListOf("때니", "빡기성", "민재몬", "이준상")
+
+    fun addItem(newItem: String) {
+        items.add(newItem)
+    }
+
+    fun getItems(): List<String> {
+        return items
     }
 }
