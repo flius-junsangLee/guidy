daa22557253e8136f246701ed4225ae9036c03b4
JunsangLee <juoc03@gmail.com>
2024-05-10 18:24:50 +0900

    1

--- app/src/main/java/com/flius/guidy/MainActivity.kt
+++ app/src/main/java/com/flius/guidy/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         profileList()
 
     }
